4745f5e3a6cf3dfacc7c4b8f766d04a7a5ba9be6
Anton Lee <anton.j.15.le@gmail.com>
2020-06-01 14:59:28 +1200

    Implement game over

--- src/GameMenu/GameMenu.js
+++ src/GameMenu/GameMenu.js
@@ -1,62 +1,58 @@
 import React from 'react';
 import { Paper, Button } from '@material-ui/core';
 import logo from '../logo.svg'
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
 import SkipNext from '@material-ui/icons/SkipNext';
 import ArrowBack from '@material-ui/icons/ArrowBack';
-import VolumeOff from '@material-ui/icons/VolumeOff';
-import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
 import Slider from '@material-ui/core/Slider';
 import VolumeDown from '@material-ui/icons/VolumeDown';
 import VolumeUp from '@material-ui/icons/VolumeUp';
 
-
-
+// GameMenu offers controls and displays a prompt
 function GameMenu(props) {
   return (
-      <Paper className="GameMenu">
-        <img className={"Logo"} src={logo} alt="Gothello the game" width="100%"></img>
-        <Grid className={"Controls"} container spacing={2}>
-          <Grid item xs={12} alignContent={"center"}>
-            <h2 style={{"textAlign": "center"}}>Your turn</h2>
-          </Grid>
-        </Grid>
-        <Grid className={"Controls"} container spacing={2}>
-          <Grid item>
-            <Button
-              variant="contained"
-              color="primary"
-              startIcon={<ArrowBack/>}
-            >
-              Resign
-            </Button>
-          </Grid>
-          <Grid item>
-            <Button color="default" startIcon={<SkipNext/>}>
-              Pass
-            </Button>
-          </Grid>
+    <Paper className="GameMenu">
+      {/* Logo */}
+      <img className={"Logo"} src={logo} alt="Gothello the game" width="100%" />
 
-          <Grid item xs >
-            <Grid item container spacing={2}>
-              <Grid item>
-                <VolumeDown />
-              </Grid>
-              <Grid item xs>
-                <Slider aria-labelledby="continuous-slider" />
-              </Grid>
-              <Grid item>
-                <VolumeUp />
-              </Grid>
+      {/* Prompt */}
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <h2 style={{ "textAlign": "center" }}>{props.prompt}</h2>
+        </Grid>
+      </Grid>
+
+      {/* Controls */}
+      <Grid className={"Controls"} container spacing={2}>
+        <Grid item>
+          <Button
+            onClick={props.resignGame}
+            variant="contained"
+            color="primary"
+            startIcon={<ArrowBack />}>
+            Resign
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button onClick={props.passTurn} color="default" startIcon={<SkipNext />}>
+            Pass
+          </Button>
+        </Grid>
+        <Grid item xs md={6} >
+          <Grid item container spacing={2}>
+            <Grid item>
+              <VolumeDown />
+            </Grid>
+            <Grid item xs>
+              <Slider aria-labelledby="continuous-slider" />
+            </Grid>
+            <Grid item>
+              <VolumeUp />
             </Grid>
           </Grid>
         </Grid>
-      </Paper>)
+      </Grid>
+    </Paper>)
 }
 
 export default GameMenu;
--- src/Routes/Game.js
+++ src/Routes/Game.js
@@ -2,17 +2,20 @@ import React, { useState, useEffect } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Board from '../Board/Board'
 import GameMenu from '../GameMenu/GameMenu';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import { SERVER_URL_WS } from '../Config';
 import { withSnackbar } from 'notistack';
 
 
 function Game(props) {
+
   let { gameId } = useParams();
+  const history = useHistory();
   let [ws, setWs] = useState(null)
   let [error, setError] = useState(false)
   let [loading, setLoading] = useState(true)
   var [board, setBoard] = useState(null)
+  var [prompt, setPrompt] = useState("Waiting for player to join")
 
   // This is a react hook it takes care of the websocket and their messages
   useEffect(() => {
@@ -25,13 +28,13 @@ function Game(props) {
       props.enqueueSnackbar("Connection failed, try refreshing", { variant: 'error' })
       setError(true)
     }
-    ws.onclose = reportError
     ws.onerror = reportError
 
     // Handles the messages that get sent
     ws.onmessage = e => {
       var msg = JSON.parse(e.data)
       switch (msg.messageType) {
+
         // Something went wrong!
         case "error":
           props.enqueueSnackbar(msg.errorMessage, { variant: 'error' });
@@ -41,10 +44,26 @@ function Game(props) {
         // We set the local state to match that of the server
         case "state":
           setBoard(msg.board)
+
+          if (msg.yourTurn) {
+            setPrompt("Your turn")
+          } else {
+            setPrompt("Their turn")
+          }
           if (error) setError(false)
           if (loading) setLoading(false)
           break;
-
+        
+        // If the game is over we inform the winner
+        case "gameOver":
+          if (msg.isWinner) {
+            setPrompt("You win, you did good")
+          } else if (msg.winner === "DRAW") {
+            setPrompt("You happy, no one wins")
+          } else {
+            setPrompt("You lost, you'll get there")
+          }
+          break;
         default:
           console.warn(msg)
           break;
@@ -54,12 +73,30 @@ function Game(props) {
 
   // CellClick tells the server that the player wants to play a stone
   function cellClick(e) {
-    ws.send(JSON.stringify(
-      {
-        "messageType": "playStone",
-        "row": e.cell.y,
-        "col": e.cell.x
-      }))
+    if (ws.readyState === WebSocket.OPEN)
+      ws.send(JSON.stringify(
+        {
+          "messageType": "playStone",
+          "row": e.cell.y,
+          "col": e.cell.x
+        }))
+  }
+
+  // PassTurn tells the server that the player is passing there turn
+  function passTurn() {
+    console.log("Pass turn")
+    if (ws.readyState === WebSocket.OPEN)
+      ws.send(JSON.stringify({ "messageType": "pass" }))
+  }
+
+  // ResignGame tells the server that the player has forfeited
+  function resignGame() {
+    console.log("Resign Game")
+    if (ws.readyState === WebSocket.OPEN) {
+      ws.send(JSON.stringify({ "messageType": "resign" }))
+      ws.close();
+    }
+    history.push("/")
   }
 
   // Displays the game board and game menu using responsive design
@@ -67,15 +104,15 @@ function Game(props) {
     <Grid container spacing={3} justify='center'>
       <Grid item xs={12} md={4} lg={4}>
         <div className={"VerticalCenter"}>
-          <GameMenu></GameMenu>
+          <GameMenu passTurn={passTurn} resignGame={resignGame} prompt={prompt}></GameMenu>
         </div>
       </Grid>
       <Grid item xs={12} sm={11} md={8} lg={6}>
         <div className={"VerticalCenter"}>
-        <Board onClick={cellClick}
-          board={board}
-          loading={loading}
-          error={error}></Board>
+          <Board onClick={cellClick}
+            board={board}
+            loading={loading}
+            error={error}></Board>
         </div>
       </Grid>
     </Grid>
