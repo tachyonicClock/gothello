3f4bf16edadeda92f1bbc652677a44bb4f7142b1
Anton Lee <anton.j.15.le@gmail.com>
2020-06-03 17:30:56 +1200

    Correct endpoint configuration

--- src/Config.js
+++ src/Config.js
@@ -1,2 +1,18 @@
-export const SERVER_URL = process.env.REACT_APP_GOTHELLO_API
-export const SERVER_URL_WS = process.env.REACT_APP_GOTHELLO_WS_API
+
+var server_url = process.env.REACT_APP_GOTHELLO_API
+var server_url_ws = process.env.REACT_APP_GOTHELLO_WS_API
+
+// If we are in the production environment then our endpoint is the same as
+// the servers
+if (server_url === "PROD") {
+  server_url = window.location.origin + "/api/v0"
+}
+if (server_url_ws === "PROD"){
+  if (window.location.protocol === "https:") {
+    server_url_ws = "wss://" + window.location.host + "/api/v0"
+  }else {
+    server_url_ws = "ws://" + window.location.host + "/api/v0"
+  }
+}
+export const SERVER_URL =  server_url
+export const SERVER_URL_WS = server_url_ws
