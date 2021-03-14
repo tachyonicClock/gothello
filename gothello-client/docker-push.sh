04a1f40050373a4d194eb8481b638759e38c5cb3
Anton Lee <anton.j.15.le@gmail.com>
2021-03-14 14:46:09 +1300

    Add project docker-compose

--- gothello-client/docker-push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# tachyonic is my name on docker hub
-img="tachyonic/gothello:client-$(git rev-parse --short HEAD)"
-docker build -t $img .
-docker push $img
--- gothello-server/docker-push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# tachyonic is my name on docker hub
-img="tachyonic/gothello:server-$(git rev-parse --short HEAD)"
-docker build -t $img .
-docker push $img
